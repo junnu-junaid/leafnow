3e31d907f1d273aece2092cbf25974c77f0a2a42
junaid pasha <77298923+junnu-junaid@users.noreply.github.com>
2021-10-03 18:47:09 +0530

    Add files via upload

---
+++ checkout.js
@@ -0,0 +1,13 @@
+
+function myFunction() {
+  var txt;
+  if (confirm("payment successful!")) {
+    txt = "You press OK!";
+  } else {
+    txt = "You pressed Cancel!";
+  }
+
+}
+
+
+
---
+++ donate.js
@@ -0,0 +1,10 @@
+function myFunctio() {
+    var txt;
+    if (confirm("your form has been submitted will contact you soon!")) {
+      txt = "You press OK!";
+    } else {
+      txt = "You pressed Cancel!";
+    }
+  
+  }
+  
